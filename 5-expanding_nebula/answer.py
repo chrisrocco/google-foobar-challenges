fb0943224b9529abd4e9243639e364b9089ab157
Chris Rocco <chris.rocco7@gmail.com>
2018-05-06 11:55:00 -0500

    final comments

--- 5-expanding_nebula/answer.py
+++ 5-expanding_nebula/answer.py
@@ -11,45 +11,46 @@ def answer(state):
     history = []
 
     # Hashes a sub-problem into a key for memoization
-    def key(i, j):
-        # The previous 'rows + 1' moves uniquely identify this sub-problem
-        return '{},{} => {}'.format(i, j, history[-(rows+1):])
+    def key(row, col):
+        # The last 'rows + 1' moves uniquely identify this sub-problem
+        return '{},{} => {}'.format(row, col, history[-(rows+1):])
 
     # Validates the next placement. Note that we _never_ make an invalid placement
-    def can_put(i, j, choice):
-        if i == 0 or j == 0:
+    def can_put(row, col, choice):
+        if row == 0 or col == 0:
             return 1
 
-        alive = state[i-1][j-1]
-        neighbors = board[i-1][j] + \
-                    board[i-1][j-1] + \
-                    board[i][j-1] + \
+        alive = state[row-1][col-1]
+        neighbors = board[row-1][col] + \
+                    board[row-1][col-1] + \
+                    board[row][col-1] + \
                     choice
 
         # check if this move would satisfy problem constraints
         return (neighbors == 1 and alive) or (neighbors != 1 and not alive)
 
     # Recursive function to permute possible board states
-    def put(i, j):
-        if i >= rows:  # line wrap for easier traversal
-            i = 0
-            j += 1
-        if j >= cols:  # found solution - placed valid cells on the entire board
+    def put(row, col):
+        if row >= rows:  # line wrap for easier traversal
+            row = 0
+            col += 1
+        if col >= cols:  # found solution - entire board is filled with valid cells
             return 1
 
         # DP lookup
-        if key(i, j) in memo:
-            return memo[key(i, j)]
+        if key(row, col) in memo:
+            return memo[key(row, col)]
 
+        # Try all possible options
         found = 0
-        for opt in [1, 0]:  # Try all possible options
-            if can_put(i, j, opt):
-                board[i][j] = opt
+        for opt in [1, 0]:
+            if can_put(row, col, opt):
+                board[row][col] = opt
                 history.append(opt)
-                found += put(i + 1, j)
+                found += put(row + 1, col)
                 history.pop()
 
-        memo[key(i, j)] = found
+        memo[key(row, col)] = found
         return found
 
     return put(0, 0)
