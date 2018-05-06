bd7228c92e056f6997f73e857c7767dd44f98783
Chris Rocco <chris.rocco7@gmail.com>
2018-05-06 12:05:24 -0500

    removed incomplete fuel_injection_perfection code

--- 4-fuel_injection_perfection/spec.py
+++ 4-fuel_injection_perfection/spec.py
@@ -7,7 +7,6 @@ class Test(unittest.TestCase):
     def test_foobar_examples(self):
         self.assertEqual(answer('4'), 2)
         self.assertEqual(answer('15'), 5)
-        self.assertEqual(answer('1' * 309), 5)
 
 
 if __name__ == '__main__':
